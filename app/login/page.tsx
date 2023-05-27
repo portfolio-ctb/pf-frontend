8b8ecc9171c8214a7fadd0d24f785e0d34581039
ctbinh <binh17122001@gmail.com>
2023-05-28 00:52:25 +0700

    test again

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,11 @@
-"use client"
-import React, { useState } from 'react'
-import Input from '../components/Input'
+'use client'
+import React from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
+import Input from '../components/Input'
+
 const Login = () => {
-  const [loading, setLoading] = useState(false)
+  // const [loading, setLoading] = useState(false)
   const {
     register,
     handleSubmit,
@@ -16,11 +17,13 @@ const Login = () => {
     },
   })
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {}
-
-  const socialAction = (action: string) => {
-    setLoading(true)
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data)
   }
+
+  // const socialAction = (action: string) => {
+  //   setLoading(true)
+  // }
   return (
     <div className="mx-auto lg:p-24 p-6 gap-2 bg-none">
       <h1 className="font-bold mx-auto text-3xl max-w-2xl text-center text-sky-600">
@@ -72,18 +75,6 @@ const Login = () => {
             className="block w-full border border-sky-700 rounded-md hover:border hover:border-sky-700 bg-sky-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-transparent hover:text-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
           >
             Login
-
-
-
-
-
-
-
-
-
-
-
-            
           </button>
         </div>
       </form>
